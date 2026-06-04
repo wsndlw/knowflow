3684d45175ea56c1a7e00bd9a8b0709d1364f64a
wsndlw <1580545665@qq.com>
2026-06-04 23:59:42 +0800

    feat(web): align table padding, rename improvement tab, and verify agent layout

--- apps/web/src/components/ui/table.tsx
+++ apps/web/src/components/ui/table.tsx
@@ -93,7 +93,7 @@ function TableCell({ className, ...props }: React.ComponentProps<"td">) {
     <td
       data-slot="table-cell"
       className={cn(
-        "p-2 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
+        "px-4 py-2 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
         className
       )}
       {...props}
